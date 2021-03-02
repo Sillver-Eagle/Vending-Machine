439724c97c086d8ee77d39df7b52ed8a62d61cac
Glitch (maple-wooded-parmesan) <none>
2021-03-02 21:43:37 +0000

    vending machine

---
+++ script.js
@@ -0,0 +1,15 @@
+var money = 10.00;
+var cokes = 1.50;
+var chips = 1.25;
+
+let random = Math.random();
+console.log(random);
+
+$(".buy").click(function() {
+  if(cokes - money );
+   $(".result-two").html("money");
+  if(chips - money);
+   $(".result-two").html(money);
+
+});
+   
