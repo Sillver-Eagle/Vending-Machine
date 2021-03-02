8515fbb12d8ecdfd9fde47ac5e414faf53ad7a1a
Glitch (maple-wooded-parmesan) <none>
2021-03-02 21:08:09 +0000

    🕵🍉 Checkpoint ./script.js:19084132/60

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var money = 10.00;
-var cokes = 0;
-var chips = 0;
+var cokes = 1.50;
+var chips = 1.25;
 
 let random = Math.random();
 console.log(random);
 
 $(".buy").click(function() {
+  if(cokes - money || chips - money);
   
   
   
